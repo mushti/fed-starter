2be213acd13524d4921c4c6c697213f0e38e85d2
Mujtaba Alvi <mushti@outlook.com>
2019-07-25 14:44:57 +0500

    Bug fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,8 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.setPublicPath('public_html')
-   .js('source_files/js/landing.js', 'public_html/js')
-   .sass('source_files/sass/landing.scss', 'public_html/css')
+   .js('source_files/js/script.js', 'public_html/js')
+   .sass('source_files/sass/style.scss', 'public_html/css')
    .options({
       processCssUrls: false
    });
